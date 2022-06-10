9dc1d51b960999d282789c3cfb420147ce054fc6
Phabliny <phablinymartins@gmail.com>
2022-06-10 16:56:20 -0300

    Adicionando o início da aplicação

---
+++ jpw_app/src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './css/style.css'
+import './css/ourstrap.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer class="footer">
+                <h2>Footer</h2>
+            </footer>
+        );
+    }
+}
---
+++ jpw_app/src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './css/style.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+          <h1>Header</h1>
+      </header>
+    )
+  }
+}
